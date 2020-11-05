99317af92aad2f72eb294e2378d6a0c2e761cf4e
Andrea <andrea@Andreas-MacBook-Pro.local>
2020-11-05 16:07:27 +0100

    wip: Bendtube module

--- src/tubeBend.jl
+++ src/tubeBend.jl
@@ -10,53 +10,37 @@ import physicalConstants: k, g
 
 struct Parameters
 #CarrierFluidProfile
-     T     :: Float64        # Carrier fluid temperature (T) SI units: K
-     Q     :: Float64        # Carrier fluid flow rate (Q) SI units: m3 s-1
-     mu    :: Float64        # Carrier fluid dynamic viscosity (mu) SI units: N s m-2
+     T     :: Float64                                        # Carrier fluid temperature (T) SI units: K
+     Q     :: Float64                                        # Carrier fluid flow rate (Q) SI units: m3 s-1
+     mu    :: Float64                                        # Carrier fluid dynamic viscosity (mu) SI units: N s m-2
 end
 struct Properties
 #Fluid
-     ro_d  :: Float64        # Droplet density (ro_d) SI units: kg m-3
-     mu_d  :: Float64        # Droplet dynamic viscosity (mu_d) SI units: N s m-2
-     d_d    :: Float64        # Droplet diameter (d_d) SI units: m
+     ro_d  :: Float64                                        # Droplet density (ro_d) SI units: kg m-3
+     mu_d  :: Float64                                        # Droplet dynamic viscosity (mu_d) SI units: N s m-2
+     d_d   :: Float64                                        # Droplet diameter (d_d) SI units: m
 #Tube
-     Rb    :: Float64        # Radius of the bend (Rb) SI units: m
-     theta :: Float64        # Bend angle (theta) SI units: radians
-     d     :: Float64        # Tube inner diameter (d) SI units: m   
+     Rb    :: Float64                                        # Radius of the bend (Rb) SI units: m
+     theta :: Float64                                        # Bend angle (theta) SI units: radians
+     d     :: Float64                                        # Tube inner diameter (d) SI units: m   
 end
 
-function AerosolPenetration(d, Rb, Q)
-     Rt = d/2                # Tube inner radius (d) SI units: m
-     Ro = Rb/Rt              # Curvature ratio (Ro) SI units: dimensionless
-     U = 4*Q/(pi*d^2)        # Fluid flow mean velocity (U) SI units: m s-1
+function AerosolPenetration(d :: Properties,
+                           Rb :: Properties,
+                            Q :: Parameters) :: Float64
+     Rt = d/2                                                # Tube inner radius (d) SI units: m
+     Ro = Rb/Rt                                              # Curvature ratio (Ro) SI units: dimensionless
+     U = 4*Q/(pi*d^2)                                        # Fluid flow mean velocity (U) SI units: m s-1
 end
 
-function Cunninghamslipcorrection(mu_d)
-
+function Cunninghamslipcorrection(mu :: Parameters,
+                                   T :: Parameters,
+                                 d_d :: Properties) :: Float64
+     lambda = (mu/p)*sqrt((pi*k*T)/(2*MM))                   # Model for the mean free path of the carrier fluid (lambda)
+     C = 1 + (lambda/dd)*(2.34+1.05*exp(-.39*d_d/lambda))    #Cunningham slip correction (C)
 end
 
 #Debug test
 # println(k)
 
 end
-
-#struct TemperatureProfile
-#    T0 :: Float64     # initial temperature
-#    Tf :: Float64     # final temperature
-#    t1 :: Float64     # time point 
-#    a :: Float64      # coefficients of quadratics
-#    b :: Float64
-#    c :: Float64
-#    d :: Float64
-#    e :: Float64
-#    f :: Float64
-#    function TemperatureProfile(T0, Tf, t1)
-#        a = T0
-#        b = 0
-#        c = (Tf-T0)/t1
-#        d = (Tf*t1 - T0) / (t1 - 1)
-#        e = (2*T0 - 2*Tf) / (t1 - 1)
-#        f = (Tf - T0) / (t1 - 1)
-#        new(T0, Tf, t1, a, b, c, d, e, f)
-#    end
-#end
