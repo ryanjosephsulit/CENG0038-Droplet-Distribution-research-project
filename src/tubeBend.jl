ab8c11f554b680785f445762a946fb0e10ace76d
Andrea <andrea@Andreas-MacBook-Pro.local>
2020-11-05 12:30:13 +0100

    wip: bendtube module

--- src/tubeBend.jl
+++ src/tubeBend.jl
@@ -8,22 +8,31 @@ module tubeBend
 
 import physicalConstants: k, g
 
-struct Properties
+struct Parameters
 #CarrierFluidProfile
      T     :: Float64        # Carrier fluid temperature (T) SI units: K
      Q     :: Float64        # Carrier fluid flow rate (Q) SI units: m3 s-1
      mu    :: Float64        # Carrier fluid dynamic viscosity (mu) SI units: N s m-2
+end
+struct Properties
 #Fluid
      ro_d  :: Float64        # Droplet density (ro_d) SI units: kg m-3
      mu_d  :: Float64        # Droplet dynamic viscosity (mu_d) SI units: N s m-2
-     dd    :: Float64        # Droplet diameter (dd) SI units: m
+     d_d    :: Float64        # Droplet diameter (d_d) SI units: m
 #Tube
      Rb    :: Float64        # Radius of the bend (Rb) SI units: m
      theta :: Float64        # Bend angle (theta) SI units: radians
-     d     :: Float64        # Tube inner diameter (d) SI units: m
-function  ()
-    
+     d     :: Float64        # Tube inner diameter (d) SI units: m   
 end
+
+function AerosolPenetration(d, Rb, Q)
+     Rt = d/2                # Tube inner radius (d) SI units: m
+     Ro = Rb/Rt              # Curvature ratio (Ro) SI units: dimensionless
+     U = 4*Q/(pi*d^2)        # Fluid flow mean velocity (U) SI units: m s-1
+end
+
+function Cunninghamslipcorrection(mu_d)
+
 end
 
 #Debug test
