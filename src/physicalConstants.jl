4759a567dce6dd74abbf240736908f6c1c87f96a
Ryan Sulit <zcecrjs@ucl.ac.uk>
2020-11-05 16:09:46 +0100

    feat: updated Constants module

--- src/physicalConstants.jl
+++ src/physicalConstants.jl
@@ -1,6 +1,6 @@
 module physicalConstants
 
-export k, g
+export k, g, p, MM
 
 # Boltzmann constant (k) SI units: m2 kg s-2 K-1
 const k = 1.38064852e-23
@@ -8,10 +8,10 @@ const k = 1.38064852e-23
 # Standard gravity (g) SI units: m s-2
 const g = 9.80665
 
-# Air at atmospheric pressure (Pa) SI units: kg s^-2 m^-2
-p = 101325;
+# Air at atmospheric pressure (Pa) SI units: kg s-2 m-2
+const p = 101325;
 
-# Molar mass of air (kg mol^-1) SI units: kg mol^-1
-MM = 28.97/(1000*6.02214179e23);
+# Molar mass of air (kg mol^-1) SI units: kg mol-1
+const MM = 28.97/(1000*6.02214179e23);
 
 end
--- src/tubeStraight.jl
+++ src/tubeStraight.jl
@@ -8,7 +8,7 @@ module tubeStraight
 
 import physicalConstants: k, g
 
-struct Parameters
+mutable struct Parameters
  # CarrierFluidProfile
     T :: Float64        # Carrier fluid temperature (T) SI units: K
     Ro :: Float64       # Carrier fluid density (ro) SI units: kg m-3
@@ -16,43 +16,51 @@ struct Parameters
     mu :: Float64       # Carrier fluid dynamic viscosity (mu) SI units: N s m-2
 end
 
-struct Properties
+## Input
+# print("Enter T, Ro, Q, mu: ")
+# params = split(readline(), ",")
+
+testArray = [1,2,3,4]
+
+
+
+
+
+# print(T)
+
+
+
+
+
+mutable struct Properties
  # Droplet
-    ro_d :: Float64     # Droplet density (ro_d) SI units: kg m-3
-    mu_d :: Float64     # Droplet dynamic viscosity (mu_d) SI units: N s m-2
-    d_d :: Float64      # Droplet diameter (dd) SI units: m
+    ro_d :: Float64                         # Droplet density (ro_d) SI units: kg m-3
+    mu_d :: Float64                         # Droplet dynamic viscosity (mu_d) SI units: N s m-2
+    d_d :: Float64                          # Droplet diameter (dd) SI units: m
+    function dropletMass(ro_d, d_d)
+        md = ro_d*(4/3)*(pi*(d_d/2)^3);     # Droplet mass (md) SI units: kg
+    end
+
  # Tube
-    d :: Float64        # Tube inner diameter (d) SI units: m
-    phi :: Float64      # Inclination angle in radians (phi) SI units: rad
-    L :: Float64        # Distance from the tube inlet (L) SI units: m
+    d :: Float64                            # Tube inner diameter (d) SI units: m
+    phi :: Float64                          # Inclination angle in radians (phi) SI units: rad
+    L :: Float64                            # Distance from the tube inlet (L) SI units: m
 end
 
-function velocities(pa :: Parameters, 
-                    pr :: Properties) :: Float64
-    
-    ### Droplet mass (md) SI units: kg
-    # Droplet density (ro_d) SI units: kg m-3
-    # Droplet diameter (dd) SI units: m
-    md = pr.ro_d*(4/3)*(pi*(pr.d_d/2)^3);
 
-    ### Brownian diffusion velocity (Vb) SI units: m s-1
-    # Boltzmann constant (k) SI units: m2 kg s-2 K-1
-    # Carrier fluid temperature (T) SI units: K
-    # Droplet mass (md) SI units: kg
-    Vb = sqrt((k*pa.T)/(2*pi*md));
-
-    
-
-
-
-
-
+function velocities(k, T, md)
+    Vb = sqrt((k*T)/(2*pi*md));          # Brownian diffusion velocity (Vb) SI units: m s-1
 end
 
 #Debug test
 # println(k)
 
+
+
+
+
 end
+
 
 
 # struct TemperatureProfile
