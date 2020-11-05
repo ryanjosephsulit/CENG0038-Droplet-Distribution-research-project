0f8eca93308f6fb52b47c4823904bb9891d6d4ca
Ryan Sulit <zcecrjs@ucl.ac.uk>
2020-11-05 12:29:16 +0100

    WIP: physicalConstants + tubeStraight modules

--- src/physicalConstants.jl
+++ src/physicalConstants.jl
@@ -5,7 +5,13 @@ export k, g
 # Boltzmann constant (k) SI units: m2 kg s-2 K-1
 const k = 1.38064852e-23
 
-#Standard gravity (g) SI units: m s-2
+# Standard gravity (g) SI units: m s-2
 const g = 9.80665
+
+# Air at atmospheric pressure (Pa) SI units: kg s^-2 m^-2
+p = 101325;
+
+# Molar mass of air (kg mol^-1) SI units: kg mol^-1
+MM = 28.97/(1000*6.02214179e23);
 
 end
